d31b1f7d650be9d1fe4d171bacc36c1420df5620
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-21 12:47:23 +0100

    ordenamiento del arbol hecho

--- clases/ArbolHuffman.py
+++ clases/ArbolHuffman.py
@@ -24,21 +24,24 @@ def bubble_sort(collection):
 def orden(dict):
     '''Introduces un diccionario y 
     devuelve una lista de nodos arbol con ramas none ordenado por frecuencias'''
-    letras=[]
     lista=[]
     for j in dict.items():
         aux=ArbolNodo()
         aux.letra,aux.fre=j[0],j[1]
         lista.append(aux)
+    length = len(lista)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if lista[j].fre > lista[j + 1].fre:
+                swapped = True
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+        if not swapped:
+            break
     for i in lista:
-        print(i.letra)
-        print(i.fre)
-    '''
-    for i in dict.keys():
-        lista.append(dict[i])
-    bubble_sort(lista)
-    print(lista)
-    '''
+        print(f'{i.letra} , {i.fre}')
+    return lista
+    
 
 if __name__=='__main__':
     dict={'A':0.2,'F':0.17,'1':0.13,'3':0.21,'0':0.05,'M':0.09,'T':0.15}
