8f079dc37176a889d8c64c27d651a44a25b6ed7a
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-21 12:18:08 +0100

    bubble sort

--- clases/ArbolHuffman.py
+++ clases/ArbolHuffman.py
@@ -6,3 +6,15 @@ class ArbolNodo(object):
 
 def crear_arbol(raiz):
     '''Crea el arbol binario'''
+
+def bubble_sort(collection):
+    length = len(collection)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                swapped = True
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+        if not swapped:
+            break  # Stop iteration if the collection is sorted.
+    return collection
