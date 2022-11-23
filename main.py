32f7f96b420fb65db17cd0d84f64ce51b9373336
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-23 16:37:53 +0100

    main hecho

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from clases.ArbolHuffman import main_ej1
 
 def main():
-    main_ej1()
+    pr1=input('Que ejercicio deseas ver (1,2,3,4,5): ')
+
+    if pr1=='1':
+        main_ej1()
+    elif pr1=='2':
+        pass
+        #main2()
+    elif pr1=='3':
+        pass
+        #main3()
+    else:
+        print('Respuesta incorrecta')
 
 if __name__=='__main__':
     main()
