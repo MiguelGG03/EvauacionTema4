3ad6d1880e4ed7d8ea49ae087ed4f27a0747fa6a
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-24 20:49:50 +0100

    clase pokemon añadida

--- clases/Pokemons.py
+++ clases/Pokemons.py
@@ -1,3 +1,24 @@
+import pandas as pd
+
+class Pokemon(object):
+
+    def __init__(self):
+        self.name=None
+        self.type1=None
+        self.type2=None
+        self.total=None
+        self.hp=None
+        self.attack=None
+        self.defense=None
+        self.sp_atk=None
+        self.sp_def=None
+        self.speed=None
+        self.generation=None
+        self.legendary=None
+
+
+#Name,Type 1,Type 2,Total,HP,Attack,Defense,Sp. Atk,Sp. Def,Speed,Generation,Legendary
+
 def main_ej2():
     pass
 
