29fc61a8cc877f8a52f42a90d2f369e59affa7cd
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-24 20:43:36 +0100

    como soy incapaz de dropear # , dejo los datos asi

--- data_analisys/limpiador_de_datos.py
+++ data_analisys/limpiador_de_datos.py
@@ -4,6 +4,10 @@ def main_cleaner():
     data= pd.read_csv('data/pokemon.csv')
     print(data.columns)
     print(data.head(1))
+    print(data['#'].head(10))
+    cols=['#']
+    data.drop(columns=cols)
+    print(data.columns)
 
 
 
