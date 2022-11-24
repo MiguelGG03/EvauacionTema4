66ea2bdd9c5b1bb34c4d933924e64df5c67076cf
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-24 20:10:26 +0100

    no funciona el archivo

---
+++ data_analisys/limpiador_de_datos.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def main_cleaner():
+    data= pd.read_csv('data/pokemon.csv')
+    print('H')
+    data.head()
+    print('H')
+
+
+
+
+
+if __name__=='_main__':
+    main_cleaner()
